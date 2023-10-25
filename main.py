9515231d5b39b8ae1feaaabb9bdf7e99252ff966
sujalsda <148936917+sujalsda@users.noreply.github.com>
2023-10-25 10:44:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,17 @@
 
 import pyrogram, os, asyncio
 
-try: app_id = int(os.environ.get("app_id", "20389440"))
+try: app_id = int(os.environ.get("app_id", "24413656"))
 except Exception as app_id: print(f"⚠️ App ID Invalid {app_id}")
-try: api_hash = os.environ.get("api_hash", "a1a06a18eb9153e9dbd447cfd5da2457")
+try: api_hash = os.environ.get("api_hash", "9e189266d4bf25aefb14ce3b3ae32078")
 except Exception as api_id: print(f"⚠️ Api Hash Invalid {api_hash}")
-try: bot_token = os.environ.get("bot_token", "6564513574:AAH3Y97iqQjSlV5vKKZdGDUohlhpA-LeSbw")
+try: bot_token = os.environ.get("bot_token", "6525980889:AAEE_jG9WjOZzQYVvgm1ty3xkeykMFzV6jc")
 except Exception as bot_token: print(f"⚠️ Bot Token Invalid {bot_token}")
-try: custom_caption = os.environ.get("custom_caption", "`{file_name}`")
+try: custom_caption = os.environ.get("custom_caption", "`{file_name}` Register Deposit & Start Earning 😋
+
+Registration link: https://cooe.in/promote/#/register?c=HE31M28Y
+
+Code - HE31M28Y 10% Bonus 1st Deposit")
 except Exception as custom_caption: print(f"⚠️ Custom Caption Invalid {custom_caption}")
 
 AutoCaptionBotV1 = pyrogram.Client(
